64eca036eef5df75d7838a294bb0df2285ed8129
qq5000 <79824925+qq5000@users.noreply.github.com>
2025-08-16 23:27:08 +0800

    Update __init__.py

--- plugins.v2/p123strmhelper2/__init__.py
+++ plugins.v2/p123strmhelper2/__init__.py
@@ -434,7 +434,7 @@ class ShareStrmHelper:
 
 class P123StrmHelper(_PluginBase):
     # 插件名称
-    plugin_name = "123云盘STRM助手"
+    plugin_name = "123云盘STRM助手2"
     # 插件描述
     plugin_desc = "123云盘STRM生成一条龙服务"
     # 插件图标
